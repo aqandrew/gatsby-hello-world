195220062867f12c78674d35071dce5f8744dac7
dawneraq <dawneraq@gmail.com>
2020-04-21 17:10:09 -0700

    add about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+  <div>
+    <h1>This is an about page</h1>
+    <Link to="/">Return Home</Link>
+    <p>Call me Ishmael.</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react"
+import { Link } from "gatsby"
 
 const Counter = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <h1>Hello Gatsby!</h1>
+      <Link to="/about/" style={{ display: `block` }}>
+        About
+      </Link>
       <button onClick={() => setCount(count + 1)}>
         Number of times clicked: {count}
       </button>
