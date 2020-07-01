e119024a404782d531156f2e2214fd3789530632
dawneraq <dawneraq@gmail.com>
2020-06-30 17:02:30 -0700

    add Netlify identity widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
 const Home = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <Helmet>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Helmet>
       <h1>Hello Gatsby!</h1>
       <nav>
         <Link to="/about/">About</Link>
